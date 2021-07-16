8397dd0a58a51be4146b49065ea4c5b60de01379
Diego Pereira <diegodsp@gmail.com>
2021-07-16 09:16:34 -0300

    - Alterações para deixar mais claro no resultado o que está sendo executado.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ namespace Blockchain
     {
         static void Main()
         {
+            // criação
+            Console.WriteLine("Cria blocos...");
             Sequence blockchain = new Sequence("Genesis")
             {
                 "Teste 1",
@@ -15,34 +17,47 @@ namespace Blockchain
                 "Teste 5"
             };
 
-            // lista
+            // listagem
+            Console.WriteLine("Lista blocos...");
             foreach (var b in blockchain)
                 Console.WriteLine(b);
 
             // verifica blocos individualmente
+            Console.WriteLine("Verifica blocos individualmente...");
             for (int i = 0; i < blockchain.Count; i++)
-                Console.WriteLine($"Block #{i} = {(Block.Validate(blockchain[i]) ? "válido" : "inválido")}");
-            Console.WriteLine("Blocos individualmente válidos!");
-
-            // verifica sequência
-            blockchain.Validate();
-            Console.WriteLine("Sequência válida!");
+                Console.WriteLine($"Bloco #{i} = {(Block.Validate(blockchain[i]) ? "válido" : "inválido")}");
 
             try
             {
-                // remove 1
-                // blockchain.RemoveAt(new Random().Next(blockchain.Count));
-                // blockchain.RemoveAt(0);
-                blockchain.RemoveAt(2);
-                Console.WriteLine("Remove um item da sequência.");
-
                 // verifica sequência
+                Console.Write("Verifica sequência: ");
                 blockchain.Validate();
-                Console.WriteLine("Sequência válida!");
+                Console.WriteLine("válida!");
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                Console.WriteLine($"inválida! Erro: {ex.Message}");
+            }
+
+            // remove um item
+            Console.WriteLine("Remove um item da sequência...");
+            // remove um aleatório
+            // blockchain.RemoveAt(new Random().Next(blockchain.Count));
+            // remove o primeiro
+            // blockchain.RemoveAt(0); // problema, pois a sequência continua válida
+            // remove um do meio
+            blockchain.RemoveAt(2);  // funciona, pois perde-se a sequência de chaves
+
+            try
+            {
+                // verifica sequência
+                Console.Write("Verifica sequência: ");
+                blockchain.Validate();
+                Console.WriteLine("válida!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"inválida! Erro: {ex.Message}");
             }
         }
     }
